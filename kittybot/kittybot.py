c9148d9693248f42d9cc365bfcc433bbf44cc0d9
AngryW1sp <147386694+AngryW1sp@users.noreply.github.com>
2024-09-29 15:53:05 +0300

    Попытка номер 2

--- kittybot/kittybot.py
+++ kittybot/kittybot.py
@@ -1,38 +1,39 @@
+
 import requests
-from telebot import TeleBot
+from telebot import TeleBot, types
 
 
-bot = TeleBot(token='7497282180:AAGhCe1IgRQz68UiRH1sglSzf-Rlo995PXY')
+bot = TeleBot(token='7236465919:AAFvRoWMgL1sQXRwWKEHV0xCIeYhsI9nkqA')
 URL = 'https://api.thecatapi.com/v1/images/search'
+chat_id = 5344749587
+text = 'Привет, Я KittyBot!'
 
 
-def get_new_image():
-    response = requests.get(URL).json()
-    random_cat = response[0].get('url')
-    return random_cat
-
-
-@bot.message_handler(commands=['newcat'])
-def new_cat(message):
+@bot.message_handler(commands=['start',])
+def wakeup(message):
     chat = message.chat
-    bot.send_photo(chat.id, get_new_image())
+    chat_id = chat.id
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    button_newcat = types.KeyboardButton('/give')
+    keyboard.add(button_newcat)
+    bot.send_message(chat_id=chat_id, text='Thx for ON', reply_markup=keyboard)
 
 
-@bot.message_handler(commands=['start'])
-def wake_up(message):
-    chat = message.chat
-    name = chat.first_name
-    bot.send_message(
-        chat_id=chat.id,
-        text=f'Спасибо, что вы включили меня, {name}!'
-    )
+@bot.message_handler(commands=['give',])
+def send_citty(message):
+
+    response = requests.get(URL).json()[0].get('url')
+    bot.send_photo(chat_id=message.chat.id, photo=response)
 
 
 @bot.message_handler(content_types=['text'])
-def say_hi(message):
+def say_hello(message):
+
     chat = message.chat
     chat_id = chat.id
-    bot.send_message(chat_id=chat_id, text='Привет, я KittyBot!')
+    text = f'Привет {message.from_user.first_name}, Я KittyBot!'
+    bot.send_message(chat_id=chat_id, text=text)
 
 
-bot.polling()
+bot.infinity_polling()
--- pet_shop/pet_shop/main/models.py
+++ pet_shop/pet_shop/main/models.py
@@ -1,3 +1,88 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Supply(models.Model):
+    name = models.CharField(max_length=150, unique=True,
+                            verbose_name='Тип питания')
+    slug = models.SlugField(max_length=150, unique=True,
+                            blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'Supply'
+        verbose_name = 'Тип питание'
+        verbose_name_plural = 'Типы питания'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True,
+                            verbose_name='Наименование отдела')
+    slug = models.SlugField(max_length=150, unique=True,
+                            blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Type_category(models.Model):
+    name = models.CharField(max_length=150, unique=True,
+                            verbose_name='Наименование вида')
+    slug = models.SlugField(max_length=150, unique=True,
+                            blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'type'
+        verbose_name = 'Категория вида'
+        verbose_name_plural = 'Категории видов'
+
+    def __str__(self):
+        return self.name
+
+
+class Pets(models.Model):
+    name = models.CharField(max_length=150, unique=True,
+                            verbose_name='Наименование вида')
+    slug = models.SlugField(max_length=200, unique=True,
+                            blank=True, null=True, verbose_name='URL')
+    size = models.CharField(max_length=100, verbose_name='Размер')
+    description = models.TextField(unique=True, verbose_name='Описание')
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE)
+    type_category = models.ForeignKey(
+        to=Type_category, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0, verbose_name='Колличество')
+    supply = models.ForeignKey(to=Supply, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'pets'
+        verbose_name = 'Животное'
+        verbose_name_plural = 'Животные'
+
+    def __str__(self):
+        return self.name
+
+
+class Product_types(models.Model):
+    name = models.CharField(max_length=150, unique=True,
+                            verbose_name='Вид продукции')
+    slug = models.SlugField(max_length=150, unique=True,
+                            blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'product_types'
+        verbose_name = 'Вид продукции'
+        verbose_name_plural = 'Виды продукции'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='')
